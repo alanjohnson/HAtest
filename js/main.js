d140a6aa57550379ab44f472ce96b17b651ff449
skittlyoaten <skittlyoaten@gmail.com>
2017-05-21 09:04:47 -0500

    add subtle motion to image

--- js/main.js
+++ js/main.js
@@ -32,6 +32,27 @@ var PropCard = function() {
         );
       }
     },
+    moveWithMouse: function(e, container, target){
+      $(target).css({'transform-origin': ((e.pageX - $(container).offset().left) / $(container).width()) * 100 + '% ' + ((e.pageY - $(container).offset().top) / $(container).height()) * 100 +'%'});
+    },
+    prepSnap: function() {
+      var speed = 250;
+      var easing = mina.easeinout;
+      document.querySelectorAll( '.property_card .control' ).forEach( function( el ) {
+        var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
+          pathConfig = {
+            from : path.attr( 'd' ),
+            midpoint : el.getAttribute( 'data-path-midpoint' ),
+            to : el.getAttribute( 'data-path-to' )
+          };
+        el.addEventListener( 'click', function() {
+          path.animate( { 'path' : pathConfig.midpoint }, speed, easing, function(){
+            $(el).addClass('moveit');
+            path.animate( { 'path' : pathConfig.from }, speed, easing, function(){ $(el).removeClass('moveit'); });
+          } );
+        } );
+      } );
+   },
     addListeners: function(e) {
       $('.icon.favorite').on('click', function(e){ PropCard.toggleForm(e); });
       $('#favorites_box input:checkbox').on('change', function(e){
@@ -42,32 +63,23 @@ var PropCard = function() {
       $("#favorites_box").on("transitionend MSTransitionEnd webkitTransitionEnd oTransitionEnd", function(e){
         if ( !$(this).hasClass('active') ) { $(this).css('top','100%'); }
       });
+      $('.image')
+        .on('mouseover', function(){
+          $(this).find('img').css({'transform': 'scale(1.02)'});
+        })
+        .on('mouseout', function(){
+          $(this).find('img').css({'transform': 'scale(1)'});
+        })
+        .on('mousemove', function(e){
+          PropCard.moveWithMouse(e, $(this), $(this).find('img') );
+        });
     },
     init: function() {
       PropCard.addListeners();
+      PropCard.prepSnap();
       if ( $('#favorites_box input:checkbox:checked').length ) {
         $('.icon.favorite').addClass('active');
       }
-
-      // SnapSVG
-      var speed = 250;
-  		var easing = mina.easeinout;
-  		[].slice.call ( document.querySelectorAll( '.property_card .control' ) ).forEach( function( el ) {
-  			var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
-  				pathConfig = {
-  					from : path.attr( 'd' ),
-            midpoint : el.getAttribute( 'data-path-midpoint' ),
-  					to : el.getAttribute( 'data-path-to' )
-  				};
-  			el.addEventListener( 'click', function() {
-  				path.animate( { 'path' : pathConfig.midpoint }, speed, easing, function(){
-            $(el).addClass('moveit');
-            path.animate( { 'path' : pathConfig.from }, speed, easing, function(){ $(el).removeClass('moveit'); });
-          } );
-  			} );
-  		} );
-     // END SnapSVG
-
     }
   };
 }();
